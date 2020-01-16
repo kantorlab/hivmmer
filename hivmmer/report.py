8ddad804f47134d30a4b828bfb2ee2a3c10ef9ba
Mark Howison <mhowison@ripl.org>
2020-01-16 12:37:14 -0500

    updates to report formatting

--- hivmmer/report.py
+++ hivmmer/report.py
@@ -1,16 +1,17 @@
 """
 """
 
+import matplotlib
+matplotlib.rcParams["font.sans-serif"] = ["Nimbus Sans L", "Helvetica", "Arial"]
+
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 import pandas as pd
-import seaborn as sns
 import time
 from collections import defaultdict
 from matplotlib.ticker import FixedLocator
 from subprocess import run
-
 
 _genes = {
     "gag": 790,
@@ -25,40 +26,82 @@ _genes = {
 _end = 9417
 
 
-def plot_coverage(codonfile, outfile, min_coverage=10):
+def plot_coverage(codonfile, outfile, min_coverage=1000):
     """
     Write a PDF plot to `outfile` showing the coverage at each HXB2 position
     based on the codon counts in `codonfile`.
     """
     codons = pd.read_csv(codonfile, sep="\t", index_col="hxb2").fillna("")
     coverage = codons.groupby(level=0)["count"].sum()
+    max_coverage = coverage.max()
 
     # Initialize plot
-    sns.set_style("ticks")
-    fig, ax = plt.subplots(1, 1, figsize=(10, 4))
+    fig, ax = plt.subplots(1, 1, figsize=(10, 2))
 
     # Draw coverage
     xlim = (min(_genes.values()), _end)
-    ax.fill_between(xlim, (min_coverage, min_coverage), color="r")
-    ax.bar(coverage.index, coverage.values, width=1.0, color="k", edgecolor="none")
+    ax.fill_between(xlim, (min_coverage, min_coverage), color="gray")
+    ax.fill_between(coverage.index, coverage.values, step="pre", color="k")
 
     # Setup x axis
-    ax.set_xlabel("Gene and HXB2 Coordinates")
     ax.set_xlim(xlim)
     ax.set_xticks(list(_genes.values()))
-    ax.set_xticklabels(["{} {}".format(gene[0], str(gene[1]).rjust(4)) for gene in _genes.items()], rotation=90, fontfamily="monospace")
+    ax.set_xticklabels(list(_genes.values()), rotation=90)
+    ax2 = ax.twiny()
+    ax2.set_xlim(ax.get_xlim())
+    ax2.set_xticks(list(_genes.values()))
+    ax2.set_xticklabels(list(_genes.keys()), rotation=90)
     ax.grid(axis="x")
 
     # Setup y axis
-    ax.set_ylabel("# of Reads")
-    yticks = [0, min_coverage]
-    if coverage.max() > 1.1*min_coverage:
-        yticks.append(coverage.max())
-    ax.set_ylim(yticks[0], yticks[-1])
-    ax.set_yticks(yticks)
+    ax.set_ylim(0, max(min_coverage, max_coverage))
+    ax.set_yticks([min_coverage, max_coverage])
 
     # Finalize and write to file
-    sns.despine(trim=True)
+    plt.tight_layout()
+    plt.savefig(outfile)
+    plt.close()
+
+
+def plot_coverage_prrt(aafile, outfile, min_coverage=1000):
+    """
+    Write a PDF plot to `outfile` showing the coverage in the PR/RT/IN
+    region of the pol gene.
+    """
+    aa = pd.read_excel(aafile)
+
+    # Initialize plot
+    fig, ax = plt.subplots(1, 1, figsize=(10, 1.5))
+
+    # Serialize the X coordinates for PR, RT, IN
+    aa["x"] = aa["position"]
+    aa.loc[aa["region"] == "RT", "x"] = aa.loc[aa["region"] == "RT", "position"] + 100
+    aa.loc[aa["region"] == "IN", "x"] = aa.loc[aa["region"] == "IN", "position"] + 541
+    del aa["region"]
+    del aa["position"]
+
+    # Calculate total coverage
+    coverage = aa.groupby("x")["coverage"].sum()
+    max_coverage = coverage.max()
+
+    # Plot coverage
+    xlim = (1, 811)
+    ax.fill_between(xlim, (min_coverage, min_coverage), color="gray")
+    ax.fill_between(coverage.index, coverage.values, step="pre", color="k")
+
+    # Setup x axis
+    xticks = [1, 101, 542]
+    xlabels = ["PR", "RT", "IN"]
+    ax.set_xlim(*xlim)
+    ax.set_xticks(xticks)
+    ax.set_xticklabels(xlabels, fontsize=12)
+    ax.grid(axis="x")
+
+    # Setup y axis
+    ax.set_ylim(0, max(min_coverage, max_coverage))
+    ax.set_yticks([min_coverage, max_coverage])
+
+    # Finalize and write to file
     plt.tight_layout()
     plt.savefig(outfile)
     plt.close()
@@ -74,8 +117,7 @@ def plot_drms(aafile, drmfile, column, outfile):
     drm = drm[drm[column] == 1]
 
     # Initialize plot
-    sns.set_style("ticks")
-    fig, (ax1, ax2) = plt.subplots(nrows=2, sharex=True, figsize=(15, 5), gridspec_kw={"height_ratios": [1, 4]})
+    fig, ax = plt.subplots(1, 1, figsize=(15, 3))
 
     # Serialize the X coordinates for PR, RT, IN
     aa["x"] = aa["position"]
@@ -96,55 +138,38 @@ def plot_drms(aafile, drmfile, column, outfile):
     aa = aa.melt(id_vars=["x", "coverage"], var_name="variant", value_name="count")
     aa["frequency"] = aa["count"] / aa["coverage"]
 
-    # Top coverage plot
-    ymax = coverage.max()
-    ylim = (0, ymax)
-    ax1.set_ylim(ylim)
-    ax1.set_yticks(ylim)
-    ax1.set_yticklabels(list(map("{:,d}".format, ylim)))
-    ax1.set_ylabel("Coverage", size=14)
-    ax1.bar(coverage.index - 0.5, coverage.values, width=1.0, color="k", edgecolor="k")
-    ax1.axvline(x=100, lw=1.0, color="w")
-    ax1.axvline(x=541, lw=1.0, color="w")
-
-    # Region labels
-    ax1.get_xaxis().set_tick_params(direction="out", which="both", top="off")
-    ax1.text(1, -0.3*ymax, "Protease", fontsize="14", weight="bold")
-    ax1.text(101, -0.3*ymax, "Reverse Transcriptase", fontsize="14", weight="bold")
-    ax1.text(543, -0.3*ymax, "Integrase", fontsize=14, weight="bold")
-
     # X axis
     margin = 1.5
     xticks = [1] + list(range(10, 100, 10)) + [101] + list(range(110, 540, 10)) + [542] + list(range(551, 811, 10))
     xlabels = [1] + list(range(10, 100, 10)) + [1] + list(range(10, 440, 10)) + [1] + list(range(10, 269, 10))
-    ax2.set_xlim(1-margin, 811+margin)
-    ax2.set_xticks(xticks)
-    ax2.set_xticklabels(xlabels, fontsize=9, rotation=90)
-    ax2.set_xlabel("AA Position", size=14)
-    ax2.xaxis.set_minor_locator(FixedLocator(list(range(1, 811))))
-    ax2.xaxis.set_tick_params(direction="out", which="both", top="off")
+    ax.set_xlim(1-margin, 811+margin)
+    ax.set_xticks(xticks)
+    ax.set_xticklabels(xlabels, fontsize=9, rotation=90)
+    ax.set_xlabel("AA Position", size=14)
+    ax.xaxis.set_minor_locator(FixedLocator(list(range(1, 811))))
+    ax.xaxis.set_tick_params(direction="out")
 
     # Y axis
     yticks = [0.1, 1, 5, 10, 20, 50, 100]
-    ax2.set_ylabel("AA Frequency", size=14)
-    ax2.set_yscale("log")
-    ax2.set_ylim(0.09, 110)
-    ax2.set_yticks(yticks)
-    ax2.set_yticklabels(['%g%%' % i for i in yticks], size=14)
-    ax2.get_yaxis().set_tick_params(direction="out", which="both", top="off")
+    ax.set_ylabel("AA Frequency", size=14)
+    ax.set_yscale("log")
+    ax.set_ylim(0.09, 110)
+    ax.set_yticks(yticks)
+    ax.set_yticklabels(['%g%%' % i for i in yticks], size=14)
+    ax.get_yaxis().set_tick_params(direction="out")
 
     # Grid
-    ax2.grid(True, which="major")
-    ax2.axvline(x=100, lw=1.0, color="k")
-    ax2.axvline(x=541, lw=1.0, color="k")
+    ax.grid(True, which="major")
+    ax.axvline(x=100, lw=1.0, color="k")
+    ax.axvline(x=541, lw=1.0, color="k")
 
     # Scatterplot
-    ax2.scatter(aa["x"], 100*aa["frequency"], ec="k", alpha=0.5, marker=".", fc="none")
+    ax.scatter(aa["x"], 100*aa["frequency"], ec="k", alpha=0.5, marker=".", fc="none")
 
     # Annotate DRMs
     if len(drm) > 0:
-        ax2.scatter(drm["x"], 100*drm["frequency"], fc="r", marker=".", ec="none")
-        ax2.scatter(drm["x"], 100*drm["frequency"], ec="r", marker="o", fc="none")
+        ax.scatter(drm["x"], 100*drm["frequency"], fc="r", marker=".", ec="none")
+        ax.scatter(drm["x"], 100*drm["frequency"], ec="r", marker="o", fc="none")
 
     # Finalize and write to file
     plt.tight_layout()
@@ -158,77 +183,65 @@ def plot_drms(aafile, drmfile, column, outfile):
 
 
 def compile(fastq1, fastq2,
-            coveragefile, drmsfile, drmifile, sdrmfile,
+            coveragefile, coverageprrtfile,
+            drmsfile, drmifile, sdrmfile,
             drms, drmi, sdrm,
             workdir, outfile):
     """
     Write a PDF report to `outfile` containing coverage, DRM, and SDRM plots.
     """
 
+    name = os.path.basename(fastq1).partition("_")[0]
     st_fastq1 = os.stat(fastq1)
     st_fastq2 = os.stat(fastq2)
 
-    tex = r"""
-           \documentclass{article}[11pt]
-           \usepackage[letterpaper, margin=1in]{geometry}
-           \usepackage{graphicx}
-           \usepackage{fontspec}
-           \setmainfont{Nimbus Sans L}
-           \begin{document}
+    tex = r"""\documentclass{article}
+\usepackage[letterpaper, margin=0.5in]{geometry}
+\usepackage{graphicx}
+\usepackage{fontspec}
+\setmainfont{Nimbus Sans L}
+\usepackage{titlesec}
+\titlespacing*{\section}{0pt}{3pt}{3pt}
+\titlespacing*{\subsection}{0pt}{3pt}{3pt}
 
-           \subsection*{Input files}
+\begin{document}
+\pagenumbering{gobble}
+\small
 
-           \begin{tabular}{ll}
-           \bf FASTQ1 & %s \\
-           Size & %.1f MB \\
-           Last modified & %s \\[6pt]
-           \bf FASTQ2 & %s \\
-           Size & %.1f MB \\
-           Last modified & %s \\
-           \end{tabular}
+\section*{Dataset: %s}
+%s (%.1f MB, %s) \\
+%s (%.1f MB, %s) \\[3pt]
 
-           \subsection*{Coverage}
-           \includegraphics[width=6.5in]{%s}
+\subsection*{Coverage: Whole Genome}
+\includegraphics[width=7.5in]{%s}
 
-           \newpage
+\subsection*{Coverage: PR/RT/IN}
+\includegraphics[width=7.5in]{%s}
 
-           \subsection*{DRMs (Stanford)}
-           \includegraphics[width=6.5in]{%s}
+\footnotesize
+\subsection*{DRMs: Stanford}
+\textbf{PI}: %s $\;$ \textbf{NRTI}: %s $\;$ \textbf{NNRTI}: %s $\;$ \textbf{INSTI}: %s \\
+\includegraphics[width=7.5in]{%s}
 
-           \begin{tabular}{ll}
-           \bf PI & %s \\
-           \bf NRTI & %s \\
-           \bf NNRTI & %s \\
-           \bf INSTI & %s \\
-           \end{tabular}
+\subsection*{DRMs: IAS}
+\textbf{PI}: %s $\;$ \textbf{NRTI}: %s $\;$ \textbf{NNRTI}: %s $\;$ \textbf{INSTI}: %s \\
+\includegraphics[width=7.5in]{%s}
 
-           \subsection*{DRMs (IAS)}
-           \includegraphics[width=6.5in]{%s}
+\subsection*{SDRMs: Stanford}
+\textbf{PI}: %s $\;$ \textbf{NRTI}: %s $\;$ \textbf{NNRTI}: %s $\;$ \textbf{INSTI}: %s \\
+\includegraphics[width=7.5in]{%s}
 
-           \begin{tabular}{ll}
-           \bf PI & %s \\
-           \bf NRTI & %s \\
-           \bf NNRTI & %s \\
-           \bf INSTI & %s \\
-           \end{tabular}
-
-           \subsection*{SDRMs (Stanford)}
-           \includegraphics[width=6.5in]{%s}
-
-           \begin{tabular}{ll}
-           \bf PI & %s \\
-           \bf NRTI & %s \\
-           \bf NNRTI & %s \\
-           \bf INSTI & %s \\
-           \end{tabular}
-
-           \end{document}
-           """ % (fastq1, st_fastq1.st_size / 1048576, time.ctime(st_fastq1.st_mtime),
-                  fastq2, st_fastq2.st_size / 1048576, time.ctime(st_fastq2.st_mtime),
-                  coveragefile,
-                  drmsfile, drms.get("PI", "None"), drms.get("NRTI", "None"), drms.get("NNRTI", "None"), drms.get("INSTI", "None"),
-                  drmifile, drmi.get("PI", "None"), drmi.get("NRTI", "None"), drmi.get("NNRTI", "None"), drmi.get("INSTI", "None"),
-                  sdrmfile, sdrm.get("PI", "None"), sdrm.get("NRTI", "None"), sdrm.get("NNRTI", "None"), sdrm.get("INSTI", "None"))
+\end{document}
+""" % (name,
+       fastq1, st_fastq1.st_size / 1048576, time.strftime("%-d %b %Y", time.localtime(st_fastq1.st_mtime)),
+       fastq2, st_fastq2.st_size / 1048576, time.strftime("%-d %b %Y", time.localtime(st_fastq2.st_mtime)),
+       coveragefile, coverageprrtfile,
+       drms.get("PI", "n/a"), drms.get("NRTI", "n/a"), drms.get("NNRTI", "n/a"), drms.get("INSTI", "n/a"),
+       drmsfile,
+       drmi.get("PI", "n/a"), drmi.get("NRTI", "n/a"), drmi.get("NNRTI", "n/a"), drmi.get("INSTI", "n/a"),
+       drmifile,
+       sdrm.get("PI", "n/a"), sdrm.get("NRTI", "n/a"), sdrm.get("NNRTI", "n/a"), sdrm.get("INSTI", "n/a"),
+       sdrmfile)
 
     # Write tex to flie
     texfile = os.path.join(workdir, "report.tex")
